daea5945ea7e0f02a98d827c8d7e209d075deabd
Moh Yusril Ihza Maulana <mohyusrilihzamaulana@gmail.com>
2021-06-01 01:47:59 +0700

    Initial commit

---
+++ app/src/main/java/com/jetpack/zoomimageview/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.jetpack.zoomimageview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
